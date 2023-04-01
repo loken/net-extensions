90d2fe07a1b26b2c7413789e5e5729acfeae21e9
Gaute Løken <gautelo@gmail.com>
2023-04-01 23:34:53 +0200

    Added a SingleOrDefaultMany IEnumerable extension.

---
+++ System.Tests/Collections/EnumerableExtensionsTests.cs
@@ -0,0 +1,43 @@
+﻿namespace Loken.System.Collections;
+
+public class EnumerableExtensionsTests
+{
+	[Fact]
+	public void SingleOrDefaultMany_Empty_ReturnsDefault()
+	{
+		var enumerable = Enumerable.Empty<string>();
+		var array = Array.Empty<string>();
+
+		Assert.Null(enumerable.SingleOrDefaultMany());
+		Assert.Null(array.SingleOrDefaultMany());
+
+		Assert.Equal("def", enumerable.SingleOrDefaultMany("def"));
+		Assert.Equal("def", array.SingleOrDefaultMany("def"));
+	}
+
+	[Fact]
+	public void SingleOrDefaultMany_Multiple_ReturnsDefault()
+	{
+		var enumerable = Enumerable.Range(1, 3).Select(n => $"Item {n}");
+		var array = enumerable.ToArray();
+
+		Assert.Null(enumerable.SingleOrDefaultMany());
+		Assert.Null(array.SingleOrDefaultMany());
+
+		Assert.Equal("def", enumerable.SingleOrDefaultMany("def"));
+		Assert.Equal("def", array.SingleOrDefaultMany("def"));
+	}
+
+	[Fact]
+	public void SingleOrDefaultMany_Single_ReturnsItem()
+	{
+		var enumerable = Enumerable.Range(1, 1).Select(n => $"Item {n}");
+		var array = enumerable.ToArray();
+
+		Assert.Equal("Item 1", enumerable.SingleOrDefaultMany());
+		Assert.Equal("Item 1", array.SingleOrDefaultMany());
+
+		Assert.Equal("Item 1", enumerable.SingleOrDefaultMany("def"));
+		Assert.Equal("Item 1", array.SingleOrDefaultMany("def"));
+	}
+}
---
+++ System/Collections/EnumerableExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace Loken.System.Collections;
+
+public static class EnumerableExtensions
+{
+	/// <summary>
+	/// Returns the only element of a sequence, or a default value if the sequence is empty or if there is more than one element in the sequence.
+	/// Similar to <see cref="Enumerable.SingleOrDefault{TSource}(IEnumerable{TSource})"/>,
+	/// but differs by returning the default when there are more than one match instead of throwing.
+	/// </summary>
+	/// <param name="source">An <see cref="IEnumerable{TSource}"/> to return the single element of.</param>
+	/// <param name="defaultVal">Optional default value.</param>
+	/// <typeparam name="TSource">The type of the elements of <paramref name="source"/>.</typeparam>
+	/// <returns>The single element of the input sequence, or <value>default(TSource)</value> if the sequence contains no- or more than one elements.</returns>
+	public static TSource? SingleOrDefaultMany<TSource>(this IEnumerable<TSource> source, TSource? defaultVal = default)
+	{
+		if (source is IList<TSource> list)
+			return list.Count == 1 ? list[0] : defaultVal;
+
+		using var enumerator = source.GetEnumerator();
+
+		if (!enumerator.MoveNext())
+			return defaultVal;
+
+		var current = enumerator.Current;
+		return enumerator.MoveNext() ? defaultVal : current;
+	}
+}
