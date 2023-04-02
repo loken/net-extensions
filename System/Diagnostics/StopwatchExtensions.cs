5d30953fe9739419bbe876bb11c1812c7933320b
Gaute Løken <gautelo@gmail.com>
2023-04-02 18:32:38 +0200

    Added a small extension for Stopwatch to get the average elapsed.

---
+++ System/Diagnostics/StopwatchExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+namespace Loken.System.Diagnostics;
+
+/// <summary>
+/// <see cref="Stopwatch"/> extensions.
+/// </summary>
+public static class StopwatchExtensions
+{
+	/// <summary>
+	/// Get the average of total <see cref="Stopwatch.ElapsedMilliseconds"/> over the <paramref name="count"/>.
+	/// Useful when you loop over something and want to know how long each iteration takes on average.
+	/// </summary>
+	public static long AvgElapsedMilliseconds(this Stopwatch stopwatch, int count)
+	{
+		return count == 0 ? 0 : stopwatch.ElapsedMilliseconds / count;
+	}
+}
