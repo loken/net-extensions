0e7c5f3a25b8872e6b264363a3a2adc6cca7a4ac
Gaute Løken <gautelo@gmail.com>
2023-04-02 18:25:56 +0200

    Added a DelimitedStringTypeConverter which allow us to convert delimited strings into arrays and collections of strings. This also showcases how to extend the conversion capabilities of the TypeConverterExtensions.

---
+++ System.Tests/ComponentModel/DelimitedStringTypeConverterTests.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+
+namespace Loken.System.ComponentModel;
+
+public class DelimitedStringTypeConverterTests : IDisposable
+{
+	private TypeDescriptionProvider DescriptorProvider { get; } = TypeDescriptor.AddAttributes(typeof(ICollection<string>), new TypeConverterAttribute(typeof(DelimitedStringTypeConverter)));
+
+	public void Dispose()
+	{
+		TypeDescriptor.RemoveProvider(DescriptorProvider, typeof(ICollection<string>));
+	}
+
+	[Fact]
+	public void Convert_ToArray()
+	{
+		var expected = new[] { "one", "other" };
+
+		var actual = "one;other".Convert<string[]>();
+
+		Assert.Equal(expected, actual);
+	}
+
+	[Fact]
+	public void Convert_ToCollection()
+	{
+		var expected = new[] { "one", "other" };
+
+		var actual = "one;other".Convert<ICollection<string>>();
+
+		Assert.Equal(expected, actual);
+	}
+}
---
+++ System/ComponentModel/DelimitedStringTypeConverter.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+
+namespace Loken.System.ComponentModel;
+
+/// <summary>
+/// Converts a source string into an array of strings based on delimiters.
+/// </summary>
+public class DelimitedStringTypeConverter : TypeConverter
+{
+	/// <inheritdoc/>
+	public override bool CanConvertFrom(ITypeDescriptorContext? context, Type sourceType)
+	{
+		return sourceType == typeof(string);
+	}
+
+	/// <inheritdoc/>
+	public override bool CanConvertTo(ITypeDescriptorContext? context, [NotNullWhen(true)] Type? destinationType)
+	{
+		return destinationType == typeof(string[]);
+	}
+
+	/// <inheritdoc/>
+	public override object? ConvertFrom(ITypeDescriptorContext? context, CultureInfo? culture, object value)
+	{
+		return ((string)value).SplitDefault();
+	}
+
+	/// <inheritdoc/>
+	public override object? ConvertTo(ITypeDescriptorContext? context, CultureInfo? culture, object? value, Type destinationType)
+	{
+		return ((string?)value)?.SplitDefault();
+	}
+}
