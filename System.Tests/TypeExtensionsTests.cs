1554229cb282eeca92137e903bb91337a2d1fa24
Gaute Løken <gautelo@gmail.com>
2023-04-02 17:35:26 +0200

    Added a Type extension for creating a FriendlyName which may or may not include namespaces and/or generics. Knows how to use the typeparameter for open generics and the specific type for closed generics.

---
+++ System.Tests/TypeExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿namespace Loken.System;
+
+public class TypeExtensionsTests
+{
+	[Theory]
+	[InlineData(typeof(int), "Int32")]
+	[InlineData(typeof(double), "Double")]
+	[InlineData(typeof(string), "String")]
+	[InlineData(typeof(DateTime), "DateTime")]
+	public void FriendlyName_BaseTypes(Type type, string expectedName)
+	{
+		Assert.Equal(expectedName, type.FriendlyName());
+	}
+
+	[Theory]
+	[InlineData(typeof(Thingy<string>), false, false, "Thingy")]
+	[InlineData(typeof(Thingy<string>), true, false, "Loken.System.Thingy")]
+	[InlineData(typeof(Thingy<string>), true, true, "Loken.System.Thingy<String>")]
+	public void FriendlyName_GenericTypes(Type type, bool ns, bool generics, string expectedName)
+	{
+		Assert.Equal(expectedName, type.FriendlyName(ns, generics));
+	}
+
+	[Theory]
+	[InlineData(typeof(Thingy<>), true, true, "Loken.System.Thingy<TPart>")]
+	public void FriendlyName_OpenGenericTypes(Type type, bool ns, bool generics, string expectedName)
+	{
+		Assert.Equal(expectedName, type.FriendlyName(ns, generics));
+	}
+}
+
+internal class Thingy<TPart> { }
---
+++ System/TypeExtensions.cs
@@ -0,0 +1,48 @@
+﻿namespace Loken.System;
+
+/// <summary>
+/// Extensions for <see cref="Type"/>
+/// </summary>
+public static class TypeExtensions
+{
+	/// <summary>
+	/// Get friendly name of a type, works better with generic types than default
+	/// </summary>
+	/// <param name="type">Type to get friendly name of.</param>
+	/// <param name="ns">Should we include the ns?</param>
+	/// <param name="generics">Should we include generic type parameters?</param>
+	/// <returns>Friendly name of Type.</returns>
+	public static string FriendlyName(this Type type, bool ns = false, bool generics = false)
+	{
+		var friendlyName = type.Name;
+		if (type.IsGenericType)
+		{
+			var backtick = friendlyName.IndexOf('`');
+			if (backtick > 0)
+				friendlyName = friendlyName.Remove(backtick);
+
+			if (generics)
+			{
+				friendlyName += "<";
+
+				var typeParameters = type.GetGenericArguments();
+				for (var i = 0; i < typeParameters.Length; ++i)
+				{
+					var typeParamName = FriendlyName(typeParameters[i], false);
+					friendlyName += i == 0 ? typeParamName : "," + typeParamName;
+				}
+
+				friendlyName += ">";
+			}
+
+			if (ns)
+				friendlyName = type.Namespace + "." + friendlyName;
+		}
+		else
+		{
+			friendlyName = ns && type.FullName is not null ? type.FullName : type.Name;
+		}
+
+		return friendlyName.Replace('+', '.');
+	}
+}
