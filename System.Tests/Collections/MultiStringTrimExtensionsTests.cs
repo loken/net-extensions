56e0235ea654778cab97602171325cde06ffb8b2
Gaute Løken <gautelo@gmail.com>
2023-04-02 22:58:53 +0200

    Added a MultiStringTrimExtensions with Trim, TrimStart and TrimEnd methods similar to the ones found on String, but for IEnumerable<string> and IList<string>.

---
+++ System.Tests/Collections/MultiStringTrimExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Loken.System.Collections;
+
+public class MultiStringTrimExtensionsTests
+{
+	public static readonly string[] Strings = { "  pad-both ", "pad-start", "pad-end ", "no-pad", "", " ", "  " };
+	public static readonly string[] Trimmed = { "pad-both", "pad-start", "pad-end", "no-pad" };
+	public static readonly string[] TrimmedStart = { "pad-both ", "pad-start", "pad-end ", "no-pad" };
+	public static readonly string[] TrimmedEnd = { "  pad-both", "pad-start", "pad-end", "no-pad" };
+
+	[Fact]
+	public void Trim_StringSequence_YieldsTrimmedSequence()
+	{
+		// Since the trim for enumerable won't modify the original we can reuse across calls.
+		var input = Strings.AsEnumerable();
+
+		Assert.Equal(Trimmed, input.Trim());
+		Assert.Equal(TrimmedStart, input.TrimStart());
+		Assert.Equal(TrimmedEnd, input.TrimEnd());
+	}
+
+	[Fact]
+	public void Trim_StringList_YieldsTrimmedList()
+	{
+		// Since the trim for lists will modify the originalwe can't reuse across calls.
+		// Note: If we used ToArray() instead of ToList() we would get the original array and things would fail, so be careful.
+		Assert.Equal(Trimmed, Strings.ToList().Trim());
+		Assert.Equal(TrimmedStart, Strings.ToList().TrimStart());
+		Assert.Equal(TrimmedEnd, Strings.ToList().TrimEnd());
+	}
+}
---
+++ System/Collections/MultiStringTrimExtensions.cs
@@ -0,0 +1,127 @@
+﻿namespace Loken.System.Collections;
+
+public static class MultiStringTrimExtensions
+{
+	/// <summary>
+	/// <see cref="string.Trim"/> each of the <paramref name="strings"/>.
+	/// </summary>
+	/// <param name="trim">The character to trim.</param>
+	/// <param name="keepEmpty">Should we keep entries that are empty after trimming or skip them?</param>
+	/// <returns>A potentially shortened sequence of trimmed entries.</returns>
+	public static IEnumerable<string> Trim(this IEnumerable<string> strings, char trim = ' ', bool keepEmpty = false)
+	{
+		foreach (var str in strings)
+		{
+			var trimmed = str.Trim(trim);
+
+			if (!keepEmpty && trimmed is null or "")
+				continue;
+
+			yield return trimmed;
+		}
+	}
+
+	/// <summary>
+	/// <see cref="string.TrimStart"/> each of the <paramref name="strings"/>.
+	/// </summary>
+	/// <param name="trim">The character to trim.</param>
+	/// <param name="keepEmpty">Should we keep entries that are empty after trimming or skip them?</param>
+	/// <returns>A potentially shortened sequence of trimmed entries.</returns>
+	public static IEnumerable<string> TrimStart(this IEnumerable<string> strings, char trim = ' ', bool keepEmpty = false)
+	{
+		foreach (var str in strings)
+		{
+			var trimmed = str.TrimStart(trim);
+
+			if (!keepEmpty && trimmed is null or "")
+				continue;
+
+			yield return trimmed;
+		}
+	}
+
+	/// <summary>
+	/// <see cref="string.TrimEnd"/> each of the <paramref name="strings"/>.
+	/// </summary>
+	/// <param name="trim">The character to trim.</param>
+	/// <param name="keepEmpty">Should we keep entries that are empty after trimming or skip them?</param>
+	/// <returns>A potentially shortened sequence of trimmed entries.</returns>
+	public static IEnumerable<string> TrimEnd(this IEnumerable<string> strings, char trim = ' ', bool keepEmpty = false)
+	{
+		foreach (var str in strings)
+		{
+			var trimmed = str.TrimEnd(trim);
+
+			if (!keepEmpty && trimmed is null or "")
+				continue;
+
+			yield return trimmed;
+		}
+	}
+
+	/// <summary>
+	/// <see cref="string.Trim"/> each of the <paramref name="list"/>.
+	/// Modifies the <paramref name="list"/>.
+	/// </summary>
+	/// <param name="trim">The character to trim.</param>
+	/// <param name="keepEmpty">Should we keep entries that are empty after trimming or skip them?</param>
+	/// <returns>The modified <paramref name="list"/>.</returns>
+	public static TList Trim<TList>(this TList list, char trim = ' ', bool keepEmpty = false)
+		where TList : IList<string>
+	{
+		for (var i = list.Count - 1; i >= 0; i--)
+		{
+			var trimmed = list[i].Trim(trim);
+			if (!keepEmpty && trimmed is null or "")
+				list.RemoveAt(i);
+			else
+				list[i] = trimmed;
+		}
+
+		return list;
+	}
+
+	/// <summary>
+	/// <see cref="string.TrimStart"/> each of the <paramref name="list"/>.
+	/// Modifies the <paramref name="list"/>.
+	/// </summary>
+	/// <param name="trim">The character to trim.</param>
+	/// <param name="keepEmpty">Should we keep entries that are empty after trimming or skip them?</param>
+	/// <returns>The modified <paramref name="list"/>.</returns>
+	public static TList TrimStart<TList>(this TList list, char trim = ' ', bool keepEmpty = false)
+	where TList : IList<string>
+	{
+		for (var i = list.Count - 1; i >= 0; i--)
+		{
+			var trimmed = list[i].TrimStart(trim);
+			if (!keepEmpty && trimmed is null or "")
+				list.RemoveAt(i);
+			else
+				list[i] = trimmed;
+		}
+
+		return list;
+	}
+
+	/// <summary>
+	/// <see cref="string.TrimEnd"/> each of the <paramref name="list"/>.
+	/// Modifies the <paramref name="list"/>.
+	/// </summary>
+	/// <param name="trim">The character to trim.</param>
+	/// <param name="keepEmpty">Should we keep entries that are empty after trimming or skip them?</param>
+	/// <returns>The modified <paramref name="list"/>.</returns>
+	public static TList TrimEnd<TList>(this TList list, char trim = ' ', bool keepEmpty = false)
+	where TList : IList<string>
+	{
+		for (var i = list.Count - 1; i >= 0; i--)
+		{
+			var trimmed = list[i].TrimEnd(trim);
+			if (!keepEmpty && trimmed is null or "")
+				list.RemoveAt(i);
+			else
+				list[i] = trimmed;
+		}
+
+		return list;
+	}
+}
