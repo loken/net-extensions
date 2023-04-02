c2ca2357c985e7c28f494c439740f09ca7be44b5
Gaute Løken <gautelo@gmail.com>
2023-04-02 15:08:33 +0200

    Added some boolean extensions for working with nullable booleans.

---
+++ System/BooleanExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Loken.System;
+
+public static class BooleanExtensions
+{
+	public static bool IsTrueOrNull(this bool? source)
+	{
+		return !source.HasValue || source.Value;
+	}
+
+	public static bool IsFalseOrNull(this bool? source)
+	{
+		return !source.HasValue || !source.Value;
+	}
+}
