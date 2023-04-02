a12e6243a6057185cf8ff2150965d71b21f79d2f
Gaute Løken <gautelo@gmail.com>
2023-04-02 22:06:12 +0200

    Added StringAffixExtensions with methods for removing all or one prefix/suffix.

---
+++ System.Tests/StringAffixExtensionsTests.cs
@@ -0,0 +1,28 @@
+﻿namespace Loken.System;
+
+public class StringAffixExtensionsTests
+{
+	[Fact]
+	public void WithoutPrefix()
+	{
+		Assert.Equal("some", "s.a:some".WithoutPrefix());
+	}
+
+	[Fact]
+	public void WithoutFirstPrefix()
+	{
+		Assert.Equal("some", "s:some".WithoutFirstPrefix());
+	}
+
+	[Fact]
+	public void WithoutSuffix()
+	{
+		Assert.Equal("some", "some:s.a".WithoutSuffix());
+	}
+
+	[Fact]
+	public void WithoutLastSuffix()
+	{
+		Assert.Equal("some", "some:s".WithoutLastSuffix());
+	}
+}
