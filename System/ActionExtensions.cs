ef87c9add40306dfe899a13e0390ee172ece908a
Gaute Løken <gautelo@gmail.com>
2023-04-02 15:06:34 +0200

    Added a Do extension method which provides a way to method-chain an instance into an action on that instance and then do more stuff on the same instance.

---
+++ System/ActionExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Loken.System;
+
+/// <summary>
+/// <see cref="Action"/> extensions.
+/// </summary>
+public static class ActionExtensions
+{
+	/// <summary>
+	/// Do the <paramref name="action"/> on the <paramref name="source"/> as long as it is non-null
+	/// and return the <paramref name="source"/> to allow method chaining.
+	/// </summary>
+	public static T Do<T>(this T source, Action<T>? action)
+	{
+		action?.Invoke(source);
+
+		return source;
+	}
+}
