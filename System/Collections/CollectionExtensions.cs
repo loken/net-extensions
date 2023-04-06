6973e52d0d993be1ce12ad9cc9c72c19217050c7
Gaute Løken <gautelo@gmail.com>
2023-04-06 23:38:42 +0200

    Added RemoveRange collection extensions to match the similar AddRange collection extensions.

--- System/Collections/CollectionExtensions.cs
+++ System/Collections/CollectionExtensions.cs
@@ -24,4 +24,34 @@ public static class CollectionExtensions
 		foreach (var item in items)
 			collection.Add(item);
 	}
+
+	/// <summary>
+	/// Remove each of the <paramref name="items"/> from the <paramref name="collection"/>.
+	/// </summary>
+	/// <returns><c>true</c> when all items were removed, <c>false</c> when some items could not be removed.</returns>
+	/// <remarks>Not in any way optimized. This is purely convenience.</remarks>
+	public static bool RemoveRange<T>(this ICollection<T> collection, params T[] items)
+	{
+		var all = true;
+
+		foreach (var item in items)
+			all &= collection.Remove(item);
+
+		return all;
+	}
+
+	/// <summary>
+	/// Add each of the <paramref name="items"/> from the <paramref name="collection"/>.
+	/// </summary>
+	/// <returns><c>true</c> when all items were removed, <c>false</c> when some items could not be removed.</returns>
+	/// <remarks>Not in any way optimized. This is purely convenience.</remarks>
+	public static bool RemoveRange<T>(this ICollection<T> collection, IEnumerable<T> items)
+	{
+		var all = true;
+
+		foreach (var item in items)
+			all &= collection.Remove(item);
+
+		return all;
+	}
 }
