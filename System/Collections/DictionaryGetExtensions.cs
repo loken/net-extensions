0afb837a84da5b59798c31b14247ac47c4ba0ebb
Gaute Løken <gautelo@gmail.com>
2023-04-18 02:40:44 +0200

    Added some DictionaryGetExtensions for more easily retrieving values while providing a default than using TryGetValue.

---
+++ System/Collections/DictionaryGetExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Loken.System.Collections;
+
+/// <summary>
+/// Extensions for retrieving values from a dictionary while providing a
+/// default value or default value generator.
+/// </summary>
+public static class DictionaryGetExtensions
+{
+	/// <summary>
+	/// Get the <typeparamref name="TValue"/> stored for the <paramref name="key"/> if it exists, <paramref name="defaultValue"/> otherwise.
+	/// </summary>
+	[return: NotNullIfNotNull(nameof(defaultValue))]
+	public static TValue? Get<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue? defaultValue = default)
+	{
+		return dictionary.TryGetValue(key, out var result)
+			? result
+			: defaultValue;
+	}
+
+	/// <summary>
+	/// Get the <typeparamref name="TValue"/> stored for the <paramref name="key"/> if it exists, <paramref name="defaultGenerator"/> value otherwise.
+	/// </summary>
+	public static TValue Get<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TValue> defaultGenerator)
+	{
+		return dictionary.TryGetValue(key, out var result)
+			? result
+			: defaultGenerator();
+	}
+}
