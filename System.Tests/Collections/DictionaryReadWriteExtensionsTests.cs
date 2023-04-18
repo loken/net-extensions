3d3c1ef571da5af4954501f8b612a73fd3682dbb
Gaute Løken <gautelo@gmail.com>
2023-04-18 02:43:53 +0200

    Added DictionaryReadWriteExtensions with a feature for transforming back-and-forth between a string and dictionary of string- or object-values. Works only for some conceptually low level types.

---
+++ System.Tests/Collections/DictionaryReadWriteExtensionsTests.cs
@@ -0,0 +1,72 @@
+﻿namespace Loken.System.Collections;
+
+public class DictionaryReadWriteExtensionsTests
+{
+	[Fact]
+	public void ReadValues()
+	{
+		var source = "ANull~Str:SomeText~Text:'Some text'~Num:42~Dbl:3.1415~On:true~Off:false~Id:f43f1ec5-04d9-481d-9c10-511577366b59~List:('hi',42,3.1415,true,false)";
+
+		var dictionary = source.ReadValues();
+
+		Assert.Null(dictionary.Get("ANull"));
+		Assert.Equal("SomeText", dictionary.Get("Str"));
+		Assert.Equal("Some text", dictionary.Get("Text"));
+		Assert.Equal(42, dictionary.Get("Num"));
+		Assert.Equal(3.1415, dictionary.Get("Dbl"));
+		Assert.Equal(true, dictionary.Get("On"));
+		Assert.Equal(false, dictionary.Get("Off"));
+		Assert.Equal(Guid.Parse("f43f1ec5-04d9-481d-9c10-511577366b59"), dictionary.Get("Id"));
+		Assert.Equal(new object[] { "hi", 42, 3.1415, true, false }, dictionary.Get("List"));
+	}
+
+	[Fact]
+	public void ReadStrings()
+	{
+		var source = "ANull~Str:SomeText~Text:Some text";
+
+		var dictionary = source.ReadStrings();
+
+		Assert.Null(dictionary.Get("ANull"));
+		Assert.Equal("SomeText", dictionary.Get("Str"));
+		Assert.Equal("Some text", dictionary.Get("Text"));
+	}
+
+	[Fact]
+	public void ReadWriteStrings()
+	{
+		var dictionary = new Dictionary<string, string?>()
+		{
+			{ "ANull", null },
+			{ "Str", "SomeText" },
+			{ "Text", "Some text" },
+		};
+
+		var formatted = dictionary.WriteString();
+		var converted = formatted.ReadStrings();
+
+		Assert.Equal(dictionary, converted);
+	}
+
+	[Fact]
+	public void ReadWriteValues()
+	{
+		var dictionary = new Dictionary<string, object?>
+		{
+			{ "ANull", null },
+			{ "Str", "SomeText" },
+			{ "Text", "Some text" },
+			{ "Num", 42 },
+			{ "Dbl", 3.1415 },
+			{ "On", true },
+			{ "Off", false },
+			{ "Id", Guid.Parse("f43f1ec5-04d9-481d-9c10-511577366b59") },
+			{ "List", new object[] { "hi", 42, 3.1415, true, false } },
+		};
+
+		var formatted = dictionary.WriteString();
+		var converted = formatted.ReadValues();
+
+		Assert.Equal(dictionary, converted);
+	}
+}
---
+++ System/Collections/DictionaryReadWriteExtensions.cs
@@ -0,0 +1,110 @@
+﻿using System.Globalization;
+using Loken.System.ComponentModel;
+
+namespace Loken.System.Collections;
+
+/// <summary>
+/// Extensions for converting between a string and either a dictionary of strings or a dictionary of converted values.
+/// </summary>
+public static class DictionaryReadWriteExtensions
+{
+	/// <summary>
+	/// Read a <paramref name="formattedString"/> into a dictionary of strings.
+	/// </summary>
+	public static IDictionary<string, string?> ReadStrings(this string formattedString, char entrySeparator = '~', char pairSeparator = ':')
+	{
+		return formattedString
+			.SplitBy(entrySeparator)
+			.Select(f => f.SplitKvp(pairSeparator))
+			.ToDictionary(p => p.Key, p => p.Value);
+	}
+
+	/// <summary>
+	/// Read a <paramref name="formattedString"/> into a dictionary of converted <see cref="object"/> values.
+	/// </summary>
+	public static IDictionary<string, object?> ReadValues(this string formattedString, char entrySeparator = '~', char pairSeparator = ':')
+	{
+		return formattedString
+			.SplitBy(entrySeparator)
+			.Select(f => f.SplitKvp(pairSeparator))
+			.ToDictionary(p => p.Key, p => p.Value?.ToValue());
+	}
+
+	/// <summary>
+	/// Write the <paramref name="dictionary"/> to a formatted string.
+	/// </summary>
+	public static string WriteString(this IDictionary<string, string?> dictionary, char entrySeparator = '~', char pairSeparator = ':')
+	{
+		return dictionary
+			.Select(p => p.Value is null
+				? p.Key
+				: p.Key + pairSeparator + p.Value)
+			.Join(entrySeparator);
+	}
+
+	/// <summary>
+	/// Write the <paramref name="dictionary"/> to a formatted string.
+	/// </summary>
+	public static string WriteString(this IDictionary<string, object?> dictionary, char entrySeparator = '~', char pairSeparator = ':')
+	{
+		return dictionary
+			.Select(p => p.Value is null
+				? p.Key
+				: p.Key + pairSeparator + p.Value.ToValueString())
+			.Join(entrySeparator);
+	}
+
+	private static object ToValue(this string source)
+	{
+		// The order of attempted conversions is important!
+		if (source.TryConvert<bool>(out var boolValue))
+			return boolValue;
+		if (source.TryConvert<int>(out var intValue))
+			return intValue;
+		if (source.TryConvert<double>(out var doubleValue))
+			return doubleValue;
+
+		// Array microsyntax when wrapped in parenthesis.
+		if (source.StartsWith('(') && source.EndsWith(')'))
+			return source[1..^1].SplitBy(',').Select(s => s.ToValue()).ToArray();
+
+		// Provides a way to force a string for something that would
+		// otherwise be parsed as a boolean or number.
+		if (source.StartsWith('\'') && source.EndsWith('\''))
+			source = source[1..^1];
+
+		// Because everybody uses GUIDs!
+		if (Guid.TryParse(source, out var guidValue))
+			return guidValue;
+
+		return source;
+	}
+
+	private static string? ToValueString(this object value)
+	{
+		if (value is double decimalValue)
+			return decimalValue.ToString(CaseInsensitiveNumbers);
+
+		if (value is object[] objArray)
+			return $"({string.Join(",", objArray.Select(o => o.ToValueString()))})";
+
+		if (value is string stringValue)
+			return stringValue.StartsWith('\'') && stringValue.EndsWith('\'')
+				? stringValue
+				: "'" + stringValue + "'";
+
+		if (value is bool boolValue)
+			return boolValue.ToString().ToLowerInvariant();
+
+		return value.ToString();
+	}
+
+	private static readonly IFormatProvider CaseInsensitiveNumbers = new CultureInfo(string.Empty)
+	{
+		NumberFormat =
+		{
+			NumberDecimalSeparator = ".",
+			NumberGroupSeparator = string.Empty,
+		},
+	};
+}
