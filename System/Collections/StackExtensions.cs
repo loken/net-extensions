8f4aa6c7e08b62d91397494a5d132b045c60c611
Gaute Løken <gautelo@gmail.com>
2023-05-18 16:18:33 +0200

    Added some StackExtensions for Push which allow us to push multiple items in a single call, similar to what we have for Queue.

---
+++ System/Collections/StackExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace Loken.System.Collections;
+
+/// <summary>
+/// Basic <see cref="Queue{T}"/> extensions
+/// </summary>
+public static class StackExtensions
+{
+	/// <summary>
+	/// <see cref="Stack{T}.Push"/> each of the <paramref name="items"/> onto the stack.
+	/// </summary>
+	public static void Push<T>(this Stack<T> stack, params T[] items)
+	{
+		if (items is null)
+			return;
+
+		foreach (var item in items)
+			stack.Push(item);
+	}
+
+	/// <summary>
+	/// <see cref="Stack{T}.Push"/> each of the <paramref name="items"/> onto the stack.
+	/// </summary>
+	public static void Push<T>(this Stack<T> stack, IEnumerable<T> items)
+	{
+		if (items is null)
+			return;
+
+		foreach (var item in items)
+			stack.Push(item);
+	}
+}
