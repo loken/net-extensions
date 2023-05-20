7d9a321981bf8e4b1cf58717cd7c19bdabb3cf3e
Gaute Løken <gautelo@gmail.com>
2023-05-20 15:06:18 +0200

    The linear.Attach(multiple) now returns the inserted count.

--- System/Collections/LinearExtensions.cs
+++ System/Collections/LinearExtensions.cs
@@ -8,24 +8,33 @@ public static class LinearExtensions
 	/// <summary>
 	/// <see cref="ILinear{T}.Enqueue"/> each of the <paramref name="items"/>.
 	/// </summary>
-	public static void Attach<T>(this ILinear<T> linear, params T[] items)
+	public static int Attach<T>(this ILinear<T> linear, params T[] items)
 	{
 		if (items is null)
-			return;
+			return 0;
 
 		foreach (var item in items)
 			linear.Attach(item);
+
+		return items.Length;
 	}
 
 	/// <summary>
 	/// <see cref="ILinear{T}.Attach"/> each of the <paramref name="items"/>.
 	/// </summary>
-	public static void Attach<T>(this ILinear<T> linear, IEnumerable<T> items)
+	public static int Attach<T>(this ILinear<T> linear, IEnumerable<T> items)
 	{
 		if (items is null)
-			return;
+			return 0;
+
+		var count = 0;
 
 		foreach (var item in items)
+		{
 			linear.Attach(item);
+			count++;
+		}
+
+		return count;
 	}
 }
